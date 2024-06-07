95a1a4052f4749a7d5369ada5fbc3ee76a932e38
h.jhkim@kalmate.net <h.jhkim@kalmate.net>
2024-06-07 14:00:09 +0900

    [REST API] : @RestController / @RequestMapping

--- spring-boot-init/src/main/java/com/juhodev/springboot/spring_boot_init/CourseController.java
+++ spring-boot-init/src/main/java/com/juhodev/springboot/spring_boot_init/CourseController.java
@@ -11,6 +11,10 @@ public class CourseController {
 
     @RequestMapping("/courses")
     public List<Course> retrieveAllCourses() {
+        /* 
+         * Spring boot starter Web will automatically convert the list of courses to JSON
+         * (JacksonHttpMeessageConverter is automatically configured by Spring Boot Starter Web)
+         */
         return Arrays.asList( 
                 new Course(1, "Learn AWS", "in28minutes"),
                 new Course(2, "Learn DevOps", "in28minutes"),
