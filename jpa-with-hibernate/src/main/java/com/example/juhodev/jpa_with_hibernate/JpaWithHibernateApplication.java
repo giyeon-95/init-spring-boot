f1969e6d0d6c81a5f90a2c7d4e5db384b7ae1f6c
h.jhkim@kalmate.net <h.jhkim@kalmate.net>
2024-06-10 16:57:20 +0900

    [init project]

---
+++ jpa-with-hibernate/src/main/java/com/example/juhodev/jpa_with_hibernate/JpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.juhodev.jpa_with_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWithHibernateApplication.class, args);
+	}
+
+}
---
+++ jpa-with-hibernate/src/test/java/com/example/juhodev/jpa_with_hibernate/JpaWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juhodev.jpa_with_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
