62b43f01e4fee936b21d10dc68f6e255432a767e
h.jhkim@kalmate.net <h.jhkim@kalmate.net>
2024-06-10 17:15:58 +0900

    [SET H2 Table] 정적 URL 설정 / table 생성

--- jpa-with-hibernate/src/main/java/com/example/juhodev/jpa_with_hibernate/JpaWithHibernateApplication.java
+++ jpa-with-hibernate/src/main/java/com/example/juhodev/jpa_with_hibernate/JpaWithHibernateApplication.java
@@ -6,6 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaWithHibernateApplication {
 
+	/*
+	 * 정적 URL 구성 전, 아래 URL을 사용하여 H2 데이터베이스에 접속.
+	 * 
+	 * 실행시에 아래와 같은 로그가 출력되는데, 이는 H2 데이터베이스의 URL을 의미한다.
+	 * (실행시마다 다른 URL이 출력된다. -> 정적 URL 구성 가능)
+	 * 
+	 * terminal console : url=jdbc:h2:mem:b2239cb8-269d-4214-b2b5-1fbd3afd912a
+	 * Set JDBC URL = jdbc:h2:mem:b2239cb8-269d-4214-b2b5-1fbd3afd912a
+	 * CONNECT 
+	 */
+
+
+	 /* 
+	  * 정적 URL 구성
+	  * application.properties 에서 정적 URL 설정후, 아래 URL을 사용하여 H2 데이터베이스에 접속.
+	  * Set JDBC URL = jdbc:h2:mem:testdb
+	  * CONNECT
+	  */
+
+	  /* 
+	   * TABLE 생성
+	   * resources/schema.sql 파일에 SQL문을 작성
+	   * RE CONNECT -> TABLE 생성 확인
+	   */
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaWithHibernateApplication.class, args);
 	}
